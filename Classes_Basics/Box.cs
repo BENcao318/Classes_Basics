5ed6be2661617e0cac1f187df2e71b9084fc30b5
Benyuan Cao <cby204@gmail.com>
2022-10-04 20:41:28 -0400

    Members and constructors.

--- Classes_Basics/Box.cs
+++ Classes_Basics/Box.cs
@@ -10,9 +10,45 @@ namespace Classes_Basics
     {
         // member variables
         private int length = 3;
-        public int height;
-        public int width;
-        public int volume;
+        private int height;
+        //public int width;
+        private int volume;
+
+        public int Width { get; set; }
+        public int Volume
+        {
+            get
+            {
+                return this.length*this.height*this.Width;
+            }
+        }
+
+        public int Height
+        {
+            get { return height; }
+            set 
+            { 
+                if(value < 0)
+                {
+                    height = -value;
+                }
+                else
+                {
+                    height = value;
+                }
+                
+            }
+        }
+
+        public Box(int length, int height, int width)
+        {
+            this.length = length;
+            this.height = height;
+
+            Width = width;
+        }
+
+
 
         // Setter
         public void SetLength(int length)
@@ -30,15 +66,9 @@ namespace Classes_Basics
             return this.length;
         }
 
-        public int GetVolume()
-        {
-            return this.length * this.height * this.width;
-        }
-
-
         public void DisplayInfo()
         {
-            Console.WriteLine("Length is {0} and heigth is {1} and width is {2} and volume is {3}", length, height, width, volume = length * height * width);
+            Console.WriteLine("Length is {0} and heigth is {1} and width is {2} and volume is {3}", length, height, Width, volume = length * height * Width);
         }
     }
 }
--- Classes_Basics/Program.cs
+++ Classes_Basics/Program.cs
@@ -1,24 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Classes_Basics;
 
-Human denis = new Human("Denis", "Panjuta", 36);
-denis.IntroduceMyself();
+//Human denis = new Human("Denis", "Panjuta", 36);
+//denis.IntroduceMyself();
 
-Human michael = new Human("Mike", "Cao", 1);
-michael.IntroduceMyself();
+//Human michael = new Human("Mike", "Cao", 1);
+//michael.IntroduceMyself();
 
-Human basicHuman = new Human();
-basicHuman.IntroduceMyself();
+//Human basicHuman = new Human();
+//basicHuman.IntroduceMyself();
 
-Human Natalie = new Human("Natalie");
-Natalie.IntroduceMyself();
+//Human Natalie = new Human("Natalie");
+//Natalie.IntroduceMyself();
 
-Box box = new Box();
-//box.length = 3;
-box.SetLength(3);
+//Box box = new Box(4,4,5);
+////box.length = 3;
 
-box.height = 4;
-box.width = 5;
-Console.WriteLine("Box's volume is {0}", box.GetVolume());
+//Console.WriteLine("Box's Width is {0}", box.Width);
+//box.Width = 10;
+//Console.WriteLine("Box's Width is {0}", box.Width);
+//Console.WriteLine("Box's volume is {0}", box.Volume);
 
-box.DisplayInfo();
+//box.DisplayInfo();
+
+Members member1 = new Members();
+member1.Introducing(true);
+Console.ReadKey();
